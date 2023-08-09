eae49afaf80d4d184d8b66b17d1d35796f340d46
Raj Aryan <96521137+raj12332@users.noreply.github.com>
2023-08-09 14:31:27 +0530

    Update _footer.php

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,6 +1,6 @@
 <div class="container-fluid bg-dark text-light">
     
-    <p class="text-centre mb-0 py-4 bn">Copyright 2022| all right deserved</p>
+    <p class="text-centre mb-0 py-4 bn">Copyright 2023| all right deserved</p>
 </div>
 <style>
     .bn{
